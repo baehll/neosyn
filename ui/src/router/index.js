0e88f8ba5c3d61b4f727c2aab291ba4d28c349b0
Fabian Harnagea <fabian.harnagea@stud.uni-goettingen.de>
2023-10-23 13:53:30 +0200

    setup npm + project + router

---
+++ ui/src/router/index.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LandingPage from "../views/landingpage/LandingPage.vue"
+import Login from "../views/Login.vue"
+import Dashboard from "../views/landingpage/children/Dashboard.vue"
+import InteractionsPage from "../views/interactions/InteractionsPage.vue"
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "login",
+            component: Login
+        },
+        {
+            path: "/landingpage",
+            name: "landingpage",
+            component: LandingPage,
+            children: [
+                {
+                    path: "/",
+                    name: "dashboard",
+                    component: Dashboard
+                },
+                {
+                    path: "/explore",
+                    name: "explore"
+                },
+                {
+                    path: "/inbox",
+                    name: "inbox"
+                },
+                {
+                    path: "/analyze",
+                    name: "analyze"
+                },
+                {
+                    path: "/share",
+                    name: "share"
+                },
+                {
+                    path: "/report",
+                    name: "report"
+                },
+                {
+                    path: "/content",
+                    name: "content"
+                },
+            ]
+        },
+        {
+            path: "/interactions",
+            name: "interactions",
+            component: InteractionsPage,
+            children: [
+                {
+                    path: "chat",
+                    name: "chat"
+                }
+            ]
+        }
+    ]
+})
+
+export default router
