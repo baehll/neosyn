0d632aebc6abf0138feae846ce4a9d8f839ee697
Fabian Harnagea <fabian.harnagea@stud.uni-goettingen.de>
2023-11-15 13:50:26 +0100

    selector for multiple ig pages added

--- client/src/views/interactions/InteractionsPage.vue
+++ client/src/views/interactions/InteractionsPage.vue
@@ -1,19 +1,28 @@
 <template>
     <div class="col">
-        <template v-for="page in fbStore.pages">
-            <div class="row">
-                <b>Posts für</b> <h3>{{ page.name }}</h3>
-                <template v-for="media in page.media_objs">
-                    <IGPost style="min-width: 85vw;" :post_obj="media" />
+        <b>Posts für</b>
+        <p>
+            <select v-model="selected">
+                <option disabled>IG Seite auswählen</option>
+                <template v-for="page, index in fbStore.pages">
+                    <option :value="index">{{ page.name }}</option>
                 </template>
-            </div>
-        </template>
+            </select>
+        </p>
+        <div class="row">
+            <template v-for="media in fbStore.pages[selected].media_objs">
+                <IGPost style="min-width: 85vw;" :post_obj="media" />
+            </template>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import IGPost from '../../components/IGPost.vue';
 import { useFBStore } from '../../store/fb';
+
+const selected = ref(0)
 
 const fbStore = useFBStore()
 </script>
