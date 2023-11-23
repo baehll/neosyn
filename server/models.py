ad6a6af42d05aba3dd5b3b508378f2287e69a664
Fabian Harnagea <fabian.harnagea@stud.uni-goettingen.de>
2023-11-23 15:59:06 +0100

    password_hash column zu klein

--- server/models.py
+++ server/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
