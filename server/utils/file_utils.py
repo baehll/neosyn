f8c1fbe7f6aa169a49e30d03f5cb529b2bf93332
Fabian Harnagea <fabian.harnagea@stud.uni-goettingen.de>
2024-05-03 14:59:48 +0200

    message posting implemented

--- server/utils/file_utils.py
+++ server/utils/file_utils.py
@@ -1,11 +1,16 @@
+import os, re, shutil, json
+
 ALLOWED_LOGO_EXTENSIONS = {"svg", "jpg", "jpeg", "png", "gif"}
 ALLOWED_COMPANY_FILE_EXTENSIONS = {"csv", "docx", "doc", "html", "json", "md", "pdf", "pptx", "ppt", "tex", "txt"}
+CORRECTED_FILE_NAME = "CORRECTED_MESSAGES_{id}.csv"
+CORRECTED_FILE_NAME_PATTERN = re.compile(r"CORRECTED_MESSAGES_\d+\.csv")
+FILE_SIZE_LIMIT = 50*1024*1024 # 50 MB
 
 def allowed_logo_extensions(logoname):
     return _allowed_file(logoname, ALLOWED_LOGO_EXTENSIONS)
 
 def allowed_company_file_extensions(filename):
-    return _allowed_file(filename, ALLOWED_COMPANY_FILE_EXTENSIONS)
+    return _allowed_file(filename, ALLOWED_COMPANY_FILE_EXTENSIONS.union(ALLOWED_LOGO_EXTENSIONS))
     
 def _allowed_file(filename, extensions):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in extensions
@@ -24,3 +29,17 @@ def get_file_size(fobj):
         pass
     
     return 0
+
+def save_corrected_messages(old_msg, new_msg, folder):
+    current_id = 0
+    current_file = folder + CORRECTED_FILE_NAME.format(id = current_id)
+    if os.path.isfile(folder + current_file):
+        size = get_file_size(current_file)
+        if size > FILE_SIZE_LIMIT:
+            # current file nach id + 1 kopieren
+            for filename in os.listdir(folder):
+                if CORRECTED_FILE_NAME_PATTERN.match(filename):
+                    current_id += 1
+            shutil.copy(current_file, folder + CORRECTED_FILE_NAME.format(id = current_id))
+    with open(folder + CORRECTED_FILE_NAME.format(id=0), "a", encoding="utf-8") as f:
+        f.write(old_msg + ";" + new_msg)
