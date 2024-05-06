76df07c3ccc05914666f1b2fae5799e9fb49c01b
Fabian Harnagea <fabian.harnagea@stud.uni-goettingen.de>
2024-05-06 20:53:33 +0200

    assistant initialization

--- server/utils/assistant_utils.py
+++ server/utils/assistant_utils.py
@@ -1,4 +1,4 @@
-
+import os
 def GPTConfig():
     from server import GPTConfig
     return GPTConfig
@@ -7,24 +7,24 @@ def upload_files_to_openai(upload_folder, filenames):
     file_ids = []
     for file in filenames:
         file_ids.append(GPTConfig().CLIENT.files.create(
-            file=open(upload_folder + "/" + file, "rb"),
+            file=open(os.path.join(upload_folder, file),"rb"),
             purpose="assistants"            
         ).id)
     return file_ids
     
-def init_vector_storage(vector_store_id, file_ids):
+def init_vector_storage(vector_store_name, file_ids):
     return GPTConfig().CLIENT.beta.vector_stores.create(
-        name=vector_store_id,
+        name=vector_store_name,
         file_ids=file_ids
     )
 
 def init_assistant(upload_folder, filenames, orga):
+    if orga.vec_storage_id != "":
+        deleted_store = GPTConfig().CLIENT.beta.vector_stores.delete(vector_store_id=orga.vec_storage_id)    
+    
     # dateien hochladen 
-    file_ids = upload_files_to_openai(upload_folder + "/" + orga.folder_path, filenames)
-    
-    # vector storage initialisieren
+    file_ids = upload_files_to_openai(os.path.join(upload_folder, orga.folder_path), filenames)
     vec_storage = init_vector_storage(orga.folder_path + "_VECTOR_STORE", file_ids)
-    
     # neuen assistant erstellen
     assistant = GPTConfig().CLIENT.beta.assistants.create(
         name="",
