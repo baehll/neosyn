87c753a62c762a29a627d47f4e6ac62e970ed003
cknott <ck@cknott.net>
2024-05-09 19:14:52 +0200

    add uploadprogress indicator

---
+++ client/src/components/registration/UploadProgressIndicator.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<span v-text="userStore.name"></span>
+	</div>
+</template>
+<script>
+import { mapStores } from 'pinia';
+import { useUserStore } from '../../stores/user';
+
+
+export default {
+	name: 'UploadProgressIndicator',
+  emits: [],
+  props: {
+		uploadStarted: {
+			type: Boolean
+		}
+  },
+  data: () => {
+    return {
+
+    }
+  },
+  computed: {
+		...mapStores(useUserStore),
+  },
+  methods: {
+
+  },
+  mounted: function() {
+  },
+  created: function() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
