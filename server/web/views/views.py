10c359c4800343b47fd912e124f0a47c29f62ed6
Fabian Laptop <fabian.harnagea@stud.uni-goettingen.de>
2024-05-10 13:33:20 +0200

    required login for protected htmls

--- server/web/views/views.py
+++ server/web/views/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify, send_from_directory
+from flask_login import login_required
 import requests
 
 views = Blueprint('views', __name__)
@@ -13,10 +14,12 @@ def serve_static_assets(filename):
     return send_from_directory('static/assets', filename)
 
 @views.route("/app.html")
+@login_required
 def app_html():
     return send_from_directory("static", "app.html")
 
 @views.route("/registration.html")
+@login_required
 def registration():
     return send_from_directory("static", "registration.html")
 
