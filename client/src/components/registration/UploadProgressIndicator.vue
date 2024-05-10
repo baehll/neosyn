3dedbe941c35d489a4af16a60e805e167b8e7d9c
cknott <ck@cknott.net>
2024-05-10 08:39:51 +0200

    add upload progress bar

--- client/src/components/registration/UploadProgressIndicator.vue
+++ client/src/components/registration/UploadProgressIndicator.vue
@@ -1,21 +1,31 @@
 <template>
-	<div class="w-full h-full flex flex-col justify-end items-center">
+	<div class="w-full h-full flex flex-col justify-center  items-center">
 		<div class="w-8/12 rounded-lg bg-darkgray border border-lightgray mb-24">
 			<div class="text-white border-b border-lightgray p-4" v-text="getApostrophedName + ' ' + $t('Documents') "></div>
-			<div class="h-64"></div>
+			<div class="h-80 bt-24 mb-48 flex justify-center items-center">
+				<highlightedfile
+				class="custom-fill"
+					v-if="userStore.companyFiles.length"
+				/>
+			</div>
 		</div>
-		<div class="progressbar w-8/12 h-2 mb-2 bg-primary rounded-lg"></div>
-		<div class="mb-16 text-white">Upload progress</div>
+		<div :class="{'w-full flex flex-col items-center': true, 'invisible': !uploadStarted}">
+			<div class="progressbar w-8/12 h-2 mb-2 bg-lightgray overflow-hidden rounded-lg"></div>
+			<div class="mb-16 text-white">Upload progress</div>
+		</div>
 	</div>
 </template>
 <script>
 import { mapStores } from 'pinia';
 import { useUserStore } from '../../stores/user';
-
+import highlightedfile from '../global/highlighted-file.vue'
 
 export default {
 	name: 'UploadProgressIndicator',
   emits: [],
+	components: {
+		highlightedfile
+	},
   props: {
 		uploadStarted: {
 			type: Boolean
@@ -46,6 +56,37 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.progressbar {
+	@apply relative;
 
+	&:before {
+		content: '';
+		@apply absolute left-0 top-0 w-0 h-full bg-primary rounded-lg;
+	}
+
+&.uploading {
+		&:before {
+			animation: bar 4s infinite;
+		}
+	}
+
+	@keyframes bar {
+		0% {
+			width: 0;
+		}
+		35% {
+			width: 25%;
+		}
+		50% {
+			width: 45%;
+		}
+		90% {
+			width: 100%;
+		}
+		100% {
+			width: 100%;
+		}
+	}
+}
 </style>
 
