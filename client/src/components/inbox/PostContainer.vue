42368230fbb490217d1c0700a29781df96b32575
cknott <ck@cknott.net>
2024-05-21 17:18:11 +0200

    add messagepost data

--- client/src/components/inbox/PostContainer.vue
+++ client/src/components/inbox/PostContainer.vue
@@ -3,6 +3,11 @@
       <div class="overflow-hidden flex flex-col justify-between w-full h-full rounded-xl message gap-4">
 				<MessagePost
 					:message-post="messagePost"
+					:likes="messagePost?.likes"
+					:shares="messagePost?.shares"
+					:comments="messagePost?.comments"
+					:image="messagePost?.postMedia"
+					:content="messagePost?.postContent"
 				/>
 				<MessageInsights
 
--- client/src/services/SuggestService.js
+++ client/src/services/SuggestService.js
@@ -2,7 +2,7 @@ import * as API from './API';
 
 export default {
     generateSuggestions: (threadId) => {
-        return API.client.post(`/api/data/ai/generate_response`, {
+        return API.client.post(`/api/data/ai/generate_responses`, {
             threadId
         })
     }
