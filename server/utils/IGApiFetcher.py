7b569baaf6118b2c145951f1ed7d63664348a39f
Fabian Harnagea <fabian.harnagea@stud.uni-goettingen.de>
2024-05-23 11:12:24 +0200

    for each business_account create customer

--- server/utils/IGApiFetcher.py
+++ server/utils/IGApiFetcher.py
@@ -274,9 +274,9 @@ def getPages(access_token, user):
     return new_pages
 
 def getBusinessAccounts(access_token, page):
-    _fields = r"instagram_business_account{followers_count},followers_count"
+    _fields = r"instagram_business_account{followers_count, profile_picture_url, username},followers_count"
     db_bzaccs = db.session.execute(db.select(IGBusinessAccount).filter(IGBusinessAccount.page.has(id=page.id))).scalars().all()
-    new_bz_accs = []
+    new_bz_accs, new_customers = [], []
     
     bs_res = _getInstagramData(access_token, f"/{page.fb_id}", fields=_fields)
     payload = []
@@ -288,25 +288,21 @@ def getBusinessAccounts(access_token, page):
     db_bzacc_dict = dict([(bz.fb_id, bz) for bz in db_bzaccs])
     fb_bzacc_dict = dict([(bz["instagram_business_account"]["id"], bz) for bz in bs_res])
     
-    # Sets zur Anwendung von Mengenoperatoren
-    # db_bzacc_fb_ids = set(db_bzacc_dict.keys())
-    # fb_bzacc_ids = set(fb_bzacc_dict.keys()) 
-    
     deletable_bzacc_ids, updateable_bzacc_ids, new_bzacc_ids = _findIDDifferences(db_bzacc_dict, fb_bzacc_dict, len(db_bzaccs) > 0)
 
-    # if len(db_bzaccs) > 0:
-    #     deletable_bzacc_ids = db_bzacc_fb_ids - fb_bzacc_ids
-    #     updateable_bzacc_ids = db_bzacc_fb_ids & fb_bzacc_ids
-    
-    # new_bzacc_ids = fb_bzacc_ids - db_bzacc_fb_ids  
-    
     # aus der Antwort neue BZ_Accs erstellen
     for id in new_bzacc_ids:
         bz = fb_bzacc_dict[id]
         new_bz_acc = IGBusinessAccount(fb_id=bz["instagram_business_account"]["id"], followers_count=bz["instagram_business_account"]["followers_count"])
         page.business_accounts.append(new_bz_acc)
         page.followers_count = bz["followers_count"]
+        
+        new_customer = IGCustomer(fb_id=bz["instagram_business_account"]["id"], profile_picture_url=bz["instagram_business_account"]["profile_picture_url"], name=bz["instagram_business_account"]["username"])
+        new_bz_acc.customer = new_customer
         new_bz_accs.append(new_bz_acc)
+        new_customers.append(new_customer)
+    
+    _commitToDB(new_bz_accs + [page] + new_customers)    
     
     if len(deletable_bzacc_ids) > 0:
         db_deletable_bzaccs = [b for b in db_bzaccs if b.fb_id in deletable_bzacc_ids]
@@ -319,9 +315,10 @@ def getBusinessAccounts(access_token, page):
             fb_bzacc_body = fb_bzacc_dict[bz.fb_id]
             if fb_bzacc_body is not None:
                 bz.followers_count = fb_bzacc_body["instagram_business_account"]["followers_count"]
+                bz.customer.profile_picture_url = fb_bzacc_body["instagram_business_account"]["profile_picture_url"]
+                bz.customer.name= fb_bzacc_body["instagram_business_account"]["username"]
                 updated_bzaccs.append(bz)
         _commitToDB(updated_bzaccs)
-    _commitToDB(new_bz_accs + [page])
 
     return new_bz_accs
 
@@ -341,17 +338,8 @@ def getMedia(access_token, bz_acc):
     db_media_dict = dict([(m.fb_id, m) for m in db_medias])
     fb_media_dict = dict([(m["id"], m) for m in media_res])
     
-    # # Sets zur Anwendung von Mengenoperatoren
-    # db_media_fb_ids = set(db_media_dict.keys())
-    # fb_media_ids = set(fb_media_dict.keys()) 
-    
     deletable_media_ids, updateable_media_ids, new_media_ids = _findIDDifferences(db_media_dict, fb_media_dict, len(db_medias) > 0)
     
-    # if len(db_medias) > 0:
-    #     deletable_media_ids = db_media_fb_ids - fb_media_ids
-    #     updateable_media_ids = db_media_fb_ids & fb_media_ids
-    
-    # new_media_ids = fb_media_ids - db_media_fb_ids
     for id in new_media_ids:
         body = fb_media_dict[id]
         new_media = IGMedia(timestamp=date_parser.isoparse(body["timestamp"]), 
@@ -415,14 +403,6 @@ def getComments(access_token, media):
                 
     deletable_comment_ids, updateable_comment_ids, new_comment_ids = _findIDDifferences(db_comment_dict, fb_comment_dict, len(db_comments)>0)
 
-    # Sets zur Anwendung von Mengenoperatoren
-    # db_comments_fb_ids = set(db_comment_dict.keys())
-    # fb_comment_ids = set(fb_comment_dict.keys()) 
-    
-    # if len(db_comments) > 0:
-    #     deletable_comment_ids = db_comments_fb_ids - fb_comment_ids
-    #     updateable_comment_ids = db_comments_fb_ids & fb_comment_ids
-    # new_comment_ids = fb_comment_ids - db_comments_fb_ids
     # print("----")
     # print(deletable_comment_ids)
     # print(updateable_comment_ids)
